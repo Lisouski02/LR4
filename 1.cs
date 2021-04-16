4be6380c9606af9b02f0e6b12cf1a6fad2faef38
Николай Лисовский <82660054+Lisouski02@users.noreply.github.com>
2021-04-16 21:14:29 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static int[] BubbleSort(int[] mas)
+        {
+            int hold;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[i] > mas[j])
+                    {
+                        hold = mas[i];
+                        mas[i] = mas[j];
+                        mas[j] = hold;
+
+                    }
+                }
+            }
+            return mas;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Введите колличество элементов в массиве : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[n];
+
+            Random r = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(0, 100);
+                Console.Write(mas[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            BubbleSort(mas);
+
+            Console.WriteLine("После соритровки:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int max, min;
+
+            if (mas[0] * mas[1] < mas[n - 2] * mas[n - 1])
+            {
+                min = mas[0] * mas[1];
+                max = mas[n - 2] * mas[n - 1];
+                Console.WriteLine("Минимальное произведение 2-х элементов = " + min);
+                Console.WriteLine("Максимальное произведение 2-х элементов = " + max);
+            }
+        }
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int n;
+            Console.Write("Введите колличество элементов в массиве : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[n];
+
+
+            Random r = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = r.Next(-100, 100);
+                Console.Write(mas[i] + " ");
+            }
+            Console.WriteLine();
+            int index = Array.FindIndex(mas, g);
+            Console.WriteLine(index);
+            int sum = 0;
+            for (; index < n; index++)
+            {
+                sum += Math.Abs(mas[index]);
+
+            }
+            Console.WriteLine("Сумма всех элементов после отрицательного в модуле = " + sum);
+
+        }
+
+        static bool g(int n)
+        {
+            return n < 0;
+        }
+    }
+}
